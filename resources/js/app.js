4e2f7123d78d55f15a018da77c87a4e9ebb021fb
Edo C. Setiawan <edocsetiawan@gmail.com>
2021-01-31 14:19:51 +0700

    add route

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,29 @@
+// App.js
+
 require('./bootstrap');
+
+import Vue from 'vue'
+
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
+import LoginComponent from './components/Login.vue';
+
+const routes = [
+    {
+        name : 'login',
+        path: '/',
+        component : LoginComponent
+    }
+]
+const router = new VueRouter({ mode: 'history', routes: routes});
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
